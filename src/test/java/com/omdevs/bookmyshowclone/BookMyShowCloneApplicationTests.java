3dfa328f6a8ec2a33a4730631d264960bfc758bb
Omdev Shastri <omdevshastriwork@gmail.com>
2023-11-01 19:56:55 +0530

    initial

---
+++ src/test/java/com/omdevs/bookmyshowclone/BookMyShowCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omdevs.bookmyshowclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
